3b2c5b51c2a1e1b8c1908c2b29311b2e63db04a2
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2017-04-21 21:16:35 +0200

    Events

--- JavaFX/mp3Player/controller/MainController.java
+++ JavaFX/mp3Player/controller/MainController.java
@@ -3,6 +3,8 @@ package mp3player.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,6 +13,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController implements Initializable {
 
@@ -52,8 +55,38 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
+        configureButtons();
+        configureVolume();
+    }
 
+    private void configureVolume(){
+    	volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent arg0) {
+				System.out.println("Slider Volume");
+			}
+    	});
+    }
+    private void configureButtons(){
+    	prevButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent arg0) {
+				System.out.println("Poprzednia piosenka");
+			}
+    	});
+
+    	nextButton.setOnAction(x-> System.out.println("NEXT"));
+    	playButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event) {
+				if(playButton.isSelected()){
+					System.out.println("Play");
+				}else {
+					System.out.println("Stop");
+				}
+			}
+
+    	});
     }
 
 }
