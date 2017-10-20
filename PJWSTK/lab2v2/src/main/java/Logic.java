3309fb89ffbf7cb30b6adfa5e8eb4f9f3e476c73
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2017-10-20 21:01:10 +0200

    delete useless files

--- PJWSTK/lab2v2/src/main/java/Logic.java
+++
@@ -1,9 +0,0 @@
-public class Logic {
-    DBManager db = new DBManager();
-
-    public void printInfo() {
-
-         TvSeries object = db.getBreakingBadInfo();
-         object.getName();
-    }
-}
