9f1bfedf5cf1df2b5b1725f7893ea017a4901922
plucins <patyk@int.pl>
2018-03-23 20:08:24 +0100

    szyfr cezara

--- JavaSE/SDA/Programowanie/AlgorytmyIStrukturyDanych/src/szyfrCezara/Main.java
+++ JavaSE/SDA/Programowanie/AlgorytmyIStrukturyDanych/src/szyfrCezara/Main.java
@@ -6,24 +6,22 @@ public class Main {
         int liczba = 5;
         String tekstZaszyfrowany = "";
 
-        for(int i = 0; i < tekst.length(); i++){
+        for (int i = 0; i < tekst.length(); i++) {
             liczba = 5;
 
-            if((int)tekst.charAt(i) + liczba> 122) liczba -= 26;
-                tekstZaszyfrowany = tekstZaszyfrowany + (char)((int)tekst.charAt(i) + liczba);
+            if ((int) tekst.charAt(i) + liczba > 122) liczba -= 26;
+            tekstZaszyfrowany = tekstZaszyfrowany + (char) ((int) tekst.charAt(i) + liczba);
         }
 
-        System.out.println("Tekst zaszyfrowany "+tekstZaszyfrowany);
+        System.out.println("Tekst zaszyfrowany " + tekstZaszyfrowany);
 
         String tekstOdszyfrowany = "";
-        for(int j = 0; j < tekstZaszyfrowany.length(); j++){
+        for (int j = 0; j < tekstZaszyfrowany.length(); j++) {
             liczba = 5;
-            if((int) tekstZaszyfrowany.charAt(j) - liczba < 97) liczba -= 26;
-            tekstOdszyfrowany = tekstOdszyfrowany + (char)((int)tekstZaszyfrowany.charAt(j) - liczba);
+            if ((int) tekstZaszyfrowany.charAt(j) - liczba < 97) liczba -= 26;
+            tekstOdszyfrowany = tekstOdszyfrowany + (char) ((int) tekstZaszyfrowany.charAt(j) - liczba);
         }
 
         System.out.println("Tekst odszyfrowany " + tekstOdszyfrowany);
-
-
     }
 }
