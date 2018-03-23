24ddbc1ebcc669570eb8b650652a80c28a480658
plucins <patyk@int.pl>
2018-03-23 20:07:08 +0100

    szyfr cezara

---
+++ JavaSE/SDA/Programowanie/AlgorytmyIStrukturyDanych/src/szyfrCezara/Controller.java
@@ -0,0 +1,5 @@
+package szyfrCezara;
+
+public class Controller {
+
+}
---
+++ JavaSE/SDA/Programowanie/AlgorytmyIStrukturyDanych/src/szyfrCezara/Main.java
@@ -0,0 +1,29 @@
+package szyfrCezara;
+
+public class Main {
+    public static void main(String[] args) {
+        String tekst = "qwertyuiop".toLowerCase();
+        int liczba = 5;
+        String tekstZaszyfrowany = "";
+
+        for(int i = 0; i < tekst.length(); i++){
+            liczba = 5;
+
+            if((int)tekst.charAt(i) + liczba> 122) liczba -= 26;
+                tekstZaszyfrowany = tekstZaszyfrowany + (char)((int)tekst.charAt(i) + liczba);
+        }
+
+        System.out.println("Tekst zaszyfrowany "+tekstZaszyfrowany);
+
+        String tekstOdszyfrowany = "";
+        for(int j = 0; j < tekstZaszyfrowany.length(); j++){
+            liczba = 5;
+            if((int) tekstZaszyfrowany.charAt(j) - liczba < 97) liczba -= 26;
+            tekstOdszyfrowany = tekstOdszyfrowany + (char)((int)tekstZaszyfrowany.charAt(j) - liczba);
+        }
+
+        System.out.println("Tekst odszyfrowany " + tekstOdszyfrowany);
+
+
+    }
+}
