05d3f137fd499bd14da1b9fc88343c4d5ccb6784
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2018-03-24 21:33:52 +0100

    palindromy

---
+++ JavaSE/SDA/Programowanie1/AlgorytmyIStrukturyDanych/src/Zadanie5/Main.java
@@ -0,0 +1,15 @@
+package Zadanie5;
+
+public class Main {
+
+//    Przygotuj algorytm wyświetlający palindrom,
+//    czyli liczbę, która czytana od tyłu będzie taka sama jak od przodu.
+
+    public static void main(String[] args) {
+        String wyraz = "zaradnydyndaraz";
+
+        for(int i = 0; i < wyraz.length()%2; i++){
+            System.out.println(wyraz.charAt(i) == wyraz.charAt(wyraz.length()-i-1));
+        }
+    }
+}
