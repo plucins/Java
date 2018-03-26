f87561ca48e913a3848cfcd1844524295408a1b1
plucins <patyk@int.pl>
2018-03-26 18:10:26 +0200

    code beauty

--- JavaSE/SDA/Programowanie1/AlgorytmyIStrukturyDanych/src/algorytmVigenerea/Main.java
+++ JavaSE/SDA/Programowanie1/AlgorytmyIStrukturyDanych/src/algorytmVigenerea/Main.java
@@ -4,20 +4,9 @@ public class Main {
     public static void main(String[] args) {
         String wyraz = "makeithappen";
         String haslo = "math";
-        Controller c = new Controller();
-        String szyfr = "";
-        String wyrazPoOdszyfrowaniu = "";
+        VigenerCharacterMap c = new VigenerCharacterMap();
 
-        for(int i = 0; i < wyraz.length();i++){
-            if(haslo.length() < wyraz.length()) haslo += haslo;
-            szyfr += c.getCharByInt(c.getIntByChar(wyraz.charAt(i)) + c.getIntByChar(haslo.charAt(i)));
-        }
-        System.out.println(szyfr);
+        c.decrypt(c.crypt(wyraz,haslo));
 
-        for(int i = 0; i < szyfr.length();i++){
-            wyrazPoOdszyfrowaniu += c.getCharByInt(c.getIntByChar(szyfr.charAt(i)) - c.getIntByChar(haslo.charAt(i)));
-        }
-
-        System.out.println(wyrazPoOdszyfrowaniu);
     }
 }
---
+++ JavaSE/SDA/Programowanie1/AlgorytmyIStrukturyDanych/src/algorytmVigenerea/VigenerCharacterMap.java
@@ -0,0 +1,61 @@
+package algorytmVigenerea;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VigenerCharacterMap {
+
+    private Map<Integer, Character> int2char = new HashMap<>();
+    private  Map<Character, Integer> char2int = new HashMap<>();
+    private String password;
+
+    private String getPassword() {
+        return password;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    public VigenerCharacterMap() {
+        initMap();
+    }
+
+    private void initMap(){
+        for(int i = 'a'; i < 'z'; i++) {
+            this.int2char.put(i-97,(char)i);
+            this.char2int.put((char)i,i-97);
+        }
+    }
+
+    private char getCharByInt(int key){
+        if(key < 0 ) key += 26;
+        return int2char.get(key%26);
+    }
+
+    private int getIntByChar(char key){
+        return char2int.get(key);
+    }
+
+    public String crypt(String wyraz, String password){
+        setPassword(password);
+        String szyfr ="";
+
+        for(int i = 0; i < wyraz.length();i++){
+            if(getPassword().length() < wyraz.length()) setPassword(getPassword()+getPassword());
+            szyfr += getCharByInt(getIntByChar(wyraz.charAt(i)) + getIntByChar(getPassword().charAt(i)));
+        }
+
+        System.out.println("Wiadomość po zaszyfrowaniu " +szyfr);
+        return szyfr;
+    }
+
+    public void decrypt(String szyfr){
+        String wyrazPoOdszyfrowaniu ="";
+
+        for(int i = 0; i < szyfr.length();i++){
+            wyrazPoOdszyfrowaniu += getCharByInt(getIntByChar(szyfr.charAt(i)) - getIntByChar(getPassword().charAt(i)));
+        }
+        System.out.println("Wiadomosc odszyfrowana " + wyrazPoOdszyfrowaniu);
+    }
+}
