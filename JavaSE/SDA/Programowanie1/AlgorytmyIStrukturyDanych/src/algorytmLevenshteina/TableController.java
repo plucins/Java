fd51c39e8a26210cde0e5530a4a0b7c4013ace34
plucins <patyk@int.pl>
2018-03-27 07:59:38 +0200

    algorytm Levenshteina

--- JavaSE/SDA/Programowanie1/AlgorytmyIStrukturyDanych/src/algorytmLevenshteina/TableController.java
+++ JavaSE/SDA/Programowanie1/AlgorytmyIStrukturyDanych/src/algorytmLevenshteina/TableController.java
@@ -22,49 +22,47 @@ public class TableController {
     }
 
 
-
-    public int[][] initTable(String wyraz1, String wyraz2){
+    public int[][] initTable(String wyraz1, String wyraz2) {
         setWyraz1(wyraz1);
         setWyraz2(wyraz2);
         int[][] table = new int[getWyraz1().length() + 1][getWyraz2().length() + 1];
 
-        for(int i = 0; i < getWyraz2().length()+1; i++){
+        for (int i = 0; i < getWyraz2().length() + 1; i++) {
             table[0][i] = i;
         }
 
-        for(int j = 0; j < getWyraz1().length()+1; j++){
+        for (int j = 0; j < getWyraz1().length() + 1; j++) {
             table[j][0] = j;
         }
         return table;
     }
 
-    public void fillTable(int[][] tab){
-        for(int i = 0; i < getWyraz1().length(); i++){
-            for(int j = 0; j < getWyraz2().length(); j++){
+    public void fillTable(int[][] tab) {
+        for (int i = 0; i < getWyraz1().length(); i++) {
+            for (int j = 0; j < getWyraz2().length(); j++) {
                 int liczbaNaGorze = 0;
                 int liczbaObok = 0;
                 int liczbaPoUkosie = 0;
                 int koszt = 0;
-                if(getWyraz1().charAt(i) != getWyraz2().charAt(j)) koszt = 1;
-                liczbaNaGorze = tab[i+1][j]+1;
-                liczbaObok = tab[i][j+1]+1;
-                liczbaPoUkosie = tab[i][j]+koszt;
+                if (getWyraz1().charAt(i) != getWyraz2().charAt(j)) koszt = 1;
+                liczbaNaGorze = tab[i + 1][j] + 1;
+                liczbaObok = tab[i][j + 1] + 1;
+                liczbaPoUkosie = tab[i][j] + koszt;
 
-                tab[i+1][j+1] = Math.min(liczbaNaGorze,Math.min(liczbaObok,liczbaPoUkosie));
+                tab[i + 1][j + 1] = Math.min(liczbaNaGorze, Math.min(liczbaObok, liczbaPoUkosie));
             }
         }
 
     }
 
-    public void printTable(int[][] tab){
-        for(int i = 0; i <= getWyraz1().length(); i++){
+    public void printTable(int[][] tab) {
+        for (int i = 0; i <= getWyraz1().length(); i++) {
             System.out.println();
-            for(int j = 0; j <= getWyraz2().length(); j++){
+            for (int j = 0; j <= getWyraz2().length(); j++) {
                 System.out.print(tab[i][j]);
             }
         }
     }
-
 
 
 }
